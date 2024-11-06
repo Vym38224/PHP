6186849b250ce6ca27b483fd4f986b1775c0f97a
Jaroslav Vymětal <93612235+Vym38224@users.noreply.github.com>
2024-11-06 13:24:19 +0100

    hotovo do ukolu 7

--- www2/databaze/views/others/index.php
+++ www2/databaze/views/others/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap.css">
     <link rel="stylesheet" href="bootstrap-icons.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
         body {
             padding-top: 70px;
@@ -44,8 +45,41 @@
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-3 pb-3 main-content">
         <h1 class="pb-3 border-bottom">Others</h1>
         <!-- Obsah stránky Others -->
+        <div id="user-list"></div>
     </main>
-    <script src="bootstrap.bundle.js"></script>
+    <script>
+        $(document).ready(function() {
+            $.ajax({
+                url: './users.json',
+                method: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    var userList = $('#user-list');
+                    userList.empty();
+                    if (data.length > 0) {
+                        var table = $('<table class="table table-striped"></table>');
+                        var thead = $('<thead><tr><th>ID</th><th>Name</th><th>Surname</th></tr></thead>');
+                        var tbody = $('<tbody></tbody>');
+                        data.forEach(function(user) {
+                            var row = $('<tr></tr>');
+                            row.append('<td>' + user.id + '</td>');
+                            row.append('<td>' + user.name + '</td>');
+                            row.append('<td>' + user.surname + '</td>');
+                            tbody.append(row);
+                        });
+                        table.append(thead);
+                        table.append(tbody);
+                        userList.append(table);
+                    } else {
+                        userList.append('<p>No users found.</p>');
+                    }
+                },
+                error: function() {
+                    $('#user-list').append('<p>Error loading user data.</p>');
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
